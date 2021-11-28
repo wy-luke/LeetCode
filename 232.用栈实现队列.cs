a22f18c4afd0470118ed7515727b0d4b3666b240
wy-luke <li1078797083@qq.com>
2021-11-28 19:35:17 +0800

    优化

--- 232.用栈实现队列.cs
+++ 232.用栈实现队列.cs
@@ -9,6 +9,7 @@ public class MyQueue
 {
     Stack<int> stack1;
     Stack<int> stack2;
+    int front;
 
     public MyQueue()
     {
@@ -16,23 +17,21 @@ public class MyQueue
         stack2 = new Stack<int>();
     }
 
+
+    // Stack1为队列倒序（先进后出），Stack2为队列序（先进先出）。
+    // Stack1先出的为第一个入栈的，Stack2先出的为最上面的，即栈的Peek。
+    // 定义front记录Stack1应先出的元素，用于队列的Peek
     public void Push(int x)
     {
-        if (stack2.Count == 0)
+        if (stack1.Count == 0)
         {
-            stack1.Push(x);
+            front = x;
         }
-        else
-        {
-            while (stack2.Count > 0)
-            {
-                stack1.Push(stack2.Pop());
-            }
-            stack1.Push(x);
-        }
-
+        stack1.Push(x);
     }
 
+    // Stack2的顶部元素为整体的先出元素，如果Stack2不为空，将Stack2顶部元素弹出即可
+    // 如果Stack2为空，则将Stack1依次弹入Stack2，再将Stack2顶部元素弹出
     public int Pop()
     {
         if (stack2.Count == 0)
@@ -50,15 +49,13 @@ public class MyQueue
 
     }
 
+    // 当Stack2不为空时，Stack2的顶部元素为整体的先出元素
+    // 当Stack2为空时，Stack1的front为整体的先出元素
     public int Peek()
     {
         if (stack2.Count == 0)
         {
-            while (stack1.Count > 0)
-            {
-                stack2.Push(stack1.Pop());
-            }
-            return stack2.Peek();
+            return front;
         }
         else
         {
