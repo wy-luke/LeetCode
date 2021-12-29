6b15f7b56194b9b204696b1140a31c6503fdc882
wy-luke <li1078797083@qq.com>
2021-12-29 20:51:44 +0800

    完成23

--- 23.合并k个升序链表.cs
+++ 23.合并k个升序链表.cs
@@ -20,14 +20,52 @@ public class Solution
 {
     public ListNode MergeKLists(ListNode[] lists)
     {
-        ListNode dummy = new ListNode();
-        ListNode p = dummy;
-
-        int k = lists.Length;
-        for (int i = 0; i < k; ++i)
+        if (lists.length == 0)
         {
-            if(i == 0) int min = lists[i]
+            return null;
         }
+        return Merge(lists, 0, lists.Length - 1);
+    }
+
+    public ListNode Merge(ListNode[] lists, int l, int r)
+    {
+        if (l > r) return null;
+
+        if (l == r) return lists[l];
+        if (r - 1 == l) return MergeTwoLists(lists[l], lists[r]);
+
+        int mid = (r + l) / 2;
+        return MergeTwoLists(Merge(lists, l, mid - 1), Merge(lists, mid, r));
+    }
+
+
+    public ListNode MergeTwoLists(ListNode l, ListNode r)
+    {
+        if (l == null) return r;
+        if (r == null) return l;
+        ListNode head = new ListNode(-1, l);
+        ListNode pre = head;
+        ListNode tmp;
+        while (l != null && r != null)
+        {
+            if (l.val <= r.val)
+            {
+                pre = l;
+                l = l.next;
+            }
+            else
+            {
+                pre.next = r;
+                tmp = r.next;
+                r.next = l;
+                pre = r;
+                r = tmp;
+            }
+        }
+
+        if (l == null) pre.next = r;
+
+        return head.next;
     }
 }
 // @lc code=end
