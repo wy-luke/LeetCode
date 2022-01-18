fda7cc7819ee072f79935980bb8b72b547a8c510
wy-luke <li1078797083@qq.com>
2022-01-18 11:21:28 +0800

    整理格式

--- 105.从前序与中序遍历序列构造二叉树.cs
+++ 105.从前序与中序遍历序列构造二叉树.cs
@@ -46,7 +46,6 @@ public class Solution
 
         return root;
     }
-
 }
 // @lc code=end
 
---
+++ 889.根据前序和后序遍历构造二叉树.cs
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=889 lang=csharp
+ *
+ * [889] 根据前序和后序遍历构造二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public TreeNode ConstructFromPrePost(int[] preorder, int[] postorder)
+    {
+
+    }
+}
+// @lc code=end
+
