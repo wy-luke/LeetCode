9c775e247d47cb98f7f285e80facebe5e5c39a7f
wy-luke <li1078797083@qq.com>
2022-01-27 21:00:34 +0800

    更新509

--- 509.斐波那契数.cpp
+++ 509.斐波那契数.cpp
@@ -5,26 +5,19 @@
  */
 
 // @lc code=start
-class Solution
-{
-private:
-    unordered_map<int, int> memo;
-
+class Solution {
 public:
-    int fib(int n)
-    {
+    unordered_map<int, int> memo;
+    int fib(int n) {
         if (n == 0)
             return 0;
         if (n == 1)
             return 1;
 
-        if (memo.count(n) == 0)
-        {
+        if (!memo.count(n) == 0) {
             memo[n] = fib(n - 1) + fib(n - 2);
             return memo[n];
-        }
-        else
-        {
+        } else {
             return memo[n];
         }
     }
