8dc11eaa3e537996b638c875ba41ec1ac519dba8
wy-luke <li1078797083@qq.com>
2022-01-28 23:37:56 +0800

    递归完成322

--- 322.零钱兑换.cpp
+++ 322.零钱兑换.cpp
@@ -7,28 +7,19 @@
 // @lc code=start
 class Solution {
 public:
-    unordered_map<int, int> memo;
     int coinChange(vector<int> &coins, int amount) {
-        if (amount == 0) return 0;
+        vector<int> dp(amount + 1, amount + 1);
 
-        if (amount < 0) return -1;
+        dp[0] = 0;
+        for (int i = 1; i <= amount; ++i) {
+            for (int coin : coins) {
+                if (i - coin < 0) continue;
 
-        if (memo.count(amount) != 0) {
-            return memo[amount];
+                dp[i] = min(dp[i], dp[i - coin] + 1);
+            }
         }
 
-        int res = INT_MAX;
-        for (int coin : coins) {
-            int subProblem = coinChange(coins, amount - coin);
-
-            if (subProblem == -1) continue;
-
-            res = min(res, subProblem + 1);
-        }
-
-        memo[amount] = res == INT_MAX ? -1 : res;
-
-        return memo[amount];
-    }
+        return dp[amount] == amount + 1 ? -1 : dp[amount];
+    };
 };
 // @lc code=end
