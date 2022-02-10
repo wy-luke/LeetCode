38d7fe34b7fcf123a440f462da084dd9efc45b6c
wy-luke <li1078797083@qq.com>
2022-02-11 00:02:43 +0800

    bfs完成130

--- 130.被围绕的区域.cpp
+++ 130.被围绕的区域.cpp
@@ -9,28 +9,38 @@ class Solution {
 public:
     int m, n;
 
-    void dfs(vector<vector<char>> &board, int x, int y) {
-        if (x < 0 || x >= m || y < 0 || y >= n || board[x][y] != 'O') return;
-
-        board[x][y] = 'A';
-        dfs(board, x + 1, y);
-        dfs(board, x - 1, y);
-        dfs(board, x, y + 1);
-        dfs(board, x, y - 1);
-    }
-
     void solve(vector<vector<char>> &board) {
         m = board.size();
         n = board[0].size();
 
+        queue<pair<int, int>> q;
+
         for (int i = 0; i < m; ++i) {
-            dfs(board, i, 0);
-            dfs(board, i, n - 1);
+            if (board[i][0] == 'O') q.emplace(i, 0);
+            if (board[i][n - 1] == 'O') q.emplace(i, n - 1);
         }
 
         for (int j = 1; j < n - 1; ++j) {
-            dfs(board, 0, j);
-            dfs(board, m - 1, j);
+            if (board[0][j] == 'O') q.emplace(0, j);
+            if (board[m - 1][j] == 'O') q.emplace(m - 1, j);
+        }
+
+        while (!q.empty()) {
+            int sz = q.size();
+
+            for (int i = 0; i < sz; ++i) {
+                int curx = q.front().first;
+                int cury = q.front().second;
+                q.pop();
+                if (curx < 0 || curx >= m || cury < 0 || cury >= n || board[curx][cury] != 'O') continue;
+
+                board[curx][cury] = 'A';
+
+                q.emplace(curx + 1, cury);
+                q.emplace(curx - 1, cury);
+                q.emplace(curx, cury + 1);
+                q.emplace(curx, cury - 1);
+            }
         }
 
         for (int i = 0; i < m; ++i) {
