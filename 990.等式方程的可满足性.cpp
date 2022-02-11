4fd4e1e2351bd6870dd08d4909ad2e532dbd1ffa
wy-luke <li1078797083@qq.com>
2022-02-11 15:20:10 +0800

    格式

--- 990.等式方程的可满足性.cpp
+++ 990.等式方程的可满足性.cpp
@@ -5,52 +5,50 @@
  */
 
 // @lc code=start
+class UF {
+private:
+    vector<int> parent;
+    vector<int> size;
+
+public:
+    UF(int n) {
+        parent.reserve(n);
+        size.resize(n, 1);
+        for (int i = 0; i < n; ++i) {
+            parent[i] = i;
+        }
+    }
+
+    void unite(int p, int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
+
+        if (rootP == rootQ) return;
+
+        if (size[rootP] < size[rootQ]) {
+            parent[rootP] = rootQ;
+            size[rootQ] += size[rootP];
+        } else {
+            parent[rootQ] = rootP;
+            size[rootP] += size[rootQ];
+        }
+    }
+
+    int find(int x) {
+        while (parent[x] != x) {
+            parent[x] = parent[parent[x]];
+            x = parent[x];
+        }
+        return x;
+    }
+
+    bool connected(int p, int q) {
+        return find(p) == find(q);
+    }
+};
+
 class Solution {
 public:
-    class UF {
-        vector<int> parent;
-        int count;
-        vector<int> size;
-
-    public:
-        UF(int n) {
-            count = n;
-            parent.reserve(n);
-            size.resize(n, 1);
-            for (int i = 0; i < n; ++i) {
-                parent[i] = i;
-            }
-        }
-
-        void unite(int p, int q) {
-            int rootP = find(p);
-            int rootQ = find(q);
-
-            if (rootP == rootQ) return;
-
-            if (size[rootP] < size[rootQ]) {
-                parent[rootP] = rootQ;
-                size[rootQ] += size[rootP];
-            } else {
-                parent[rootQ] = rootP;
-                size[rootP] += size[rootQ];
-            }
-            count--;
-        }
-
-        int find(int x) {
-            while (parent[x] != x) {
-                parent[x] = parent[parent[x]];
-                x = parent[x];
-            }
-            return x;
-        }
-
-        bool connected(int p, int q) {
-            return find(p) == find(q);
-        }
-    };
-
     bool equationsPossible(vector<string> &equations) {
         UF uf(26);
         for (string i : equations) {
