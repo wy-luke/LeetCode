0eab348afee8ba2037540464ffa8ce3fbdc9d163
wy-luke <li1078797083@qq.com>
2022-02-11 16:30:11 +0800

    dfs完成990

--- 990.等式方程的可满足性.cpp
+++ 990.等式方程的可满足性.cpp
@@ -5,59 +5,49 @@
  */
 
 // @lc code=start
-class UF {
-private:
-    vector<int> parent;
-    vector<int> size;
-
-public:
-    UF(int n) {
-        parent.reserve(n);
-        size.resize(n, 1);
-        for (int i = 0; i < n; ++i) {
-            parent[i] = i;
-        }
-    }
-
-    void unite(int p, int q) {
-        int rootP = find(p);
-        int rootQ = find(q);
-
-        if (rootP == rootQ) return;
-
-        if (size[rootP] < size[rootQ]) {
-            parent[rootP] = rootQ;
-            size[rootQ] += size[rootP];
-        } else {
-            parent[rootQ] = rootP;
-            size[rootP] += size[rootQ];
-        }
-    }
-
-    int find(int x) {
-        while (parent[x] != x) {
-            parent[x] = parent[parent[x]];
-            x = parent[x];
-        }
-        return x;
-    }
-
-    bool connected(int p, int q) {
-        return find(p) == find(q);
-    }
-};
-
 class Solution {
+    int dp[26][26];
+    bool notEqual[26 * 26];
+    vector<bool> visited;
+
 public:
+    bool dfs(int x, int y) {
+        if (notEqual[x * 26 + y] || visited[x * 26 + y]) return false;
+
+        if (dp[x][y] == 1) return true;
+
+        visited[x * 26 + y] = true;
+        for (int i = 0; i < 26; ++i) {
+            if (dp[x][i] == 1) {
+                if (dfs(i, y)) return true;
+            }
+        }
+        notEqual[x * 26 + y] = true;
+        return false;
+    }
+
     bool equationsPossible(vector<string> &equations) {
-        UF uf(26);
+        visited.resize(26 * 26);
+        for (int i = 0; i < 26; ++i) {
+            for (int j = 0; j < 26; ++j) {
+                if (i == j)
+                    dp[i][j] = 1;
+                else
+                    dp[i][j] = 0;
+            }
+        }
+
         for (string i : equations) {
-            if (i[1] == '=') uf.unite(i[0] - 'a', i[3] - 'a');
+            if (i[1] == '=') {
+                dp[i[0] - 'a'][i[3] - 'a'] = 1;
+                dp[i[3] - 'a'][i[0] - 'a'] = 1;
+            }
         }
 
         for (string i : equations) {
             if (i[1] == '!') {
-                if (uf.connected(i[0] - 'a', i[3] - 'a')) return false;
+                if (dfs(i[0] - 'a', i[3] - 'a')) return false;
+                fill(visited.begin(), visited.end(), false);
             }
         }
         return true;
