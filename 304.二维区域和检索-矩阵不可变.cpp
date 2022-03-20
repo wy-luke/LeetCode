0fadcd9597cff61c21f74a59aaafc5ffa6084660
wy-luke <li1078797083@qq.com>
2022-03-20 20:51:10 +0800

    完成304

---
+++ 304.二维区域和检索-矩阵不可变.cpp
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=304 lang=cpp
+ *
+ * [304] 二维区域和检索 - 矩阵不可变
+ */
+
+// @lc code=start
+class NumMatrix {
+public:
+    NumMatrix(vector<vector<int>> &matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        if (m == 0 || n == 0) return;
+
+        preSum.resize(m + 1, vector<int>(n + 1));
+
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                preSum[i][j] = preSum[i - 1][j] + preSum[i][j - 1] + matrix[i - 1][j - 1] - preSum[i - 1][j - 1];
+            }
+        }
+    }
+
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        return preSum[row2 + 1][col2 + 1] - preSum[row1][col2 + 1] - preSum[row2 + 1][col1] + preSum[row1][col1];
+    }
+
+private:
+    vector<vector<int>> preSum;
+};
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix* obj = new NumMatrix(matrix);
+ * int param_1 = obj->sumRegion(row1,col1,row2,col2);
+ */
+// @lc code=end
