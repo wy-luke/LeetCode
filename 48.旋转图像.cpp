709ae35c0b6c436cb40f21354babf3542e476404
wy-luke <li1078797083@qq.com>
2022-03-22 10:55:19 +0800

    优化48

--- 48.旋转图像.cpp
+++ 48.旋转图像.cpp
@@ -9,25 +9,17 @@ class Solution {
 public:
     void rotate(vector<vector<int>> &matrix) {
         int m = matrix.size();
-        int n = matrix[0].size();
         // 首先按对角线镜像
         for (int i = 0; i < m; ++i) {
-            for (int j = i + 1; j < n; ++j) {
-                int tmp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = tmp;
+            for (int j = i + 1; j < m; ++j) {
+                swap(matrix[i][j], matrix[j][i]);
             }
         }
 
         // 然后按行反转
-        for (int k = 0; k < m; ++k) {
-            int a = 0, b = n - 1;
-            while (a < b) {
-                int tmp = matrix[k][a];
-                matrix[k][a] = matrix[k][b];
-                matrix[k][b] = tmp;
-                a++;
-                b--;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < m / 2; ++j) {
+                swap(matrix[i][j], matrix[i][m - j - 1]);
             }
         }
     }
