b426f4548813f12d7538fcfaac1aae9988c662d8
wy-luke <li1078797083@qq.com>
2022-03-25 12:45:13 +0800

    二分完成34

---
+++ 34.在排序数组中查找元素的第一个和最后一个位置.cpp
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=34 lang=cpp
+ *
+ * [34] 在排序数组中查找元素的第一个和最后一个位置
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> searchRange(vector<int> &nums, int target) {
+        int left = 0, right = nums.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] == target) {
+                right = mid - 1;
+            }
+        }
+        if (left >= nums.size() || nums[left] != target) {
+            return {-1, -1};
+        }
+        int resLeft = left;
+        right = nums.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] == target) {
+                left = mid + 1;
+            }
+        }
+        return {resLeft, right};
+    }
+};
+// @lc code=end
