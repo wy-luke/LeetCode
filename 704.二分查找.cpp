bc9aa817b2b5281b6d99c4735fe2b843a6652b94
wy-luke <li1078797083@qq.com>
2022-03-25 10:19:10 +0800

    二分完成704

---
+++ 704.二分查找.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=704 lang=cpp
+ *
+ * [704] 二分查找
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int search(vector<int> &nums, int target) {
+        int left = 0, right = nums.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
+// @lc code=end
