eaf9e749474a28cea3979f58bf5ef1a60c80c00c
wy-luke <li1078797083@qq.com>
2022-04-07 21:13:09 +0800

    c++完成142

---
+++ 142.环形链表-ii.cpp
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=142 lang=cpp
+ *
+ * [142] 环形链表 II
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode *p1 = head, *p2 = head;
+        while (p2 != nullptr && p2->next != nullptr) {
+            p1 = p1->next;
+            p2 = p2->next->next;
+            if (p1 == p2) {
+                p1 = head;
+                while (p1 != p2) {
+                    p1 = p1->next;
+                    p2 = p2->next;
+                }
+                return p1;
+            }
+        }
+        return nullptr;
+    }
+};
+// @lc code=end
