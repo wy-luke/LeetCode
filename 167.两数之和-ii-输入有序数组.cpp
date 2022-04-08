b8b0b33dc91b894eee854c49249768f047386b83
wy-luke <li1078797083@qq.com>
2022-04-08 17:28:42 +0800

    完成167

---
+++ 167.两数之和-ii-输入有序数组.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=167 lang=cpp
+ *
+ * [167] 两数之和 II - 输入有序数组
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> twoSum(vector<int> &numbers, int target) {
+        int left = 0, right = numbers.size() - 1;
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target) {
+                return {left + 1, right + 1};
+            } else if (sum > target) {
+                right--;
+            } else if (sum < target) {
+                left++;
+            }
+        }
+        return {};
+    }
+};
+// @lc code=end
