1db66a563bdcf56b909a12162c3cea10af84dd3f
wy-luke <li1078797083@qq.com>
2022-04-08 18:36:42 +0800

    完成344

---
+++ 344.反转字符串.cpp
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=344 lang=cpp
+ *
+ * [344] 反转字符串
+ */
+
+// @lc code=start
+class Solution {
+public:
+    void reverseString(vector<char> &s) {
+        int left = 0, right = s.size() - 1;
+        while (left < right) {
+            char tmp = s[left];
+            s[left] = s[right];
+            s[right] = tmp;
+            ++left;
+            --right;
+        }
+    }
+};
+// @lc code=end
