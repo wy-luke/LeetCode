c2c6fad2f94641f879ec5e18fd5b8874668052bd
wy-luke <li1078797083@qq.com>
2022-05-15 16:23:11 +0800

    三角形面积公式+三层循环😅

---
+++ 812.最大三角形面积.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=812 lang=cpp
+ *
+ * [812] 最大三角形面积
+ */
+
+// @lc code=start
+class Solution {
+public:
+    double largestTriangleArea(vector<vector<int>> &points) {
+        int n = points.size();
+        double res = 0;
+        for (int i = 0; i < n - 2; ++i) {
+            for (int j = i + 1; j < n - 1; ++j) {
+                for (int k = j + 1; k < n; ++k) {
+                    int x1 = points[i][0];
+                    int y1 = points[i][1];
+                    int x2 = points[j][0];
+                    int y2 = points[j][1];
+                    int x3 = points[k][0];
+                    int y3 = points[k][1];
+                    res = max(res, abs(x1 * y2 + x2 * y3 + x3 * y1 - x1 * y3 - x2 * y1 - x3 * y2) * 0.5);
+                }
+            }
+        }
+        return res;
+    }
+};
+// @lc code=end
