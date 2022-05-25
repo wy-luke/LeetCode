8878f35e40f8c9abcfe892c19a7eb995b6a8c183
wy-luke <li1078797083@qq.com>
2022-05-25 18:42:58 +0800

    加上并查集优化675

--- 675.为高尔夫比赛砍树.cpp
+++ 675.为高尔夫比赛砍树.cpp
@@ -1,3 +1,4 @@
+// @lc code=start
 /*
  * @lc app=leetcode.cn id=675 lang=cpp
  *
@@ -7,42 +8,81 @@
 // @lc code=start
 class Solution {
 public:
+    // 并查集
+    vector<int> parent;
+    int find(int p) {
+        if (parent[p] != p) {
+            parent[p] = find(parent[p]);
+        }
+        return parent[p];
+    }
+
+    void unite(int p, int q) {
+        parent[find(p)] = parent[find(q)];
+    }
+
+    bool connected(int p, int q) {
+        return find(p) == find(q);
+    }
+
     // 求两点的曼哈顿距离
     int getDis(int sx, int sy, int ex, int ey) {
         return abs(ex - sx) + abs(ey - sy);
     }
 
     int cutOffTree(vector<vector<int>> &forest) {
+        // 方向数组
+        int dir[4][2] = {0, -1, -1, 0, 0, 1, 1, 0};
+
         int m = forest.size();
         int n = forest[0].size();
-        vector<pair<int, int>> tree;
+        // 初始化并查集
+        parent.reserve(m * n);
+        for (int i = 0; i < m * n; ++i) {
+            parent.emplace_back(i);
+        }
+        // 获取全部的树
+        vector<pair<int, int>> trees;
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
-                if (forest[i][j] > 1) tree.emplace_back(i, j);
+                // 把树加到数组
+                if (forest[i][j] > 1) trees.emplace_back(i, j);
+                // 开始并查集连通，如果该点为障碍，continue
+                if (forest[i][j] == 0) continue;
+                // 只检查 左方 和 上方 即可
+                for (int k = 0; k < 2; ++k) {
+                    int nx = i + dir[k][0];
+                    int ny = j + dir[k][1];
+                    // 检查过界 和 障碍
+                    if (nx < 0 || nx >= m || ny < 0 || ny >= n || forest[nx][ny] == 0) continue;
+                    unite(i * n + j, nx * n + ny);
+                }
             }
         }
+        // 如果有 树 没有和 (0,0) 连通，则返回 -1
+        for (const auto &[x, y] : trees) {
+            if (!connected(0, x * n + y)) return -1;
+        }
         // 排序技巧！！！
-        sort(tree.begin(), tree.end(), [&](const auto &a, const auto &b) {
+        sort(trees.begin(), trees.end(), [&](const auto &a, const auto &b) {
             return forest[a.first][a.second] < forest[b.first][b.second];
         });
-        // 方向数组
-        int dir[4][2] = {0, 1, 0, -1, 1, 0, -1, 0};
         // 没有递归，可使用 auto
-        function<int(int, int, int, int)> astar = [&](int startx, int starty, int endx, int endy) -> int {
+        function<int(int, int, int, int)> bfs = [&](int startx, int starty, int endx, int endy) -> int {
             if (startx == endx && starty == endy) return 0;
             int step;
             // 记录 (从起点到某点的步数 + 该点到 end 的曼哈顿距离) 和 该点二维坐标
             // 每次优先遍历能够更快到达 end 的点，即小顶堆的 top
             priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<tuple<int, int, int>>> pq;
             pq.emplace(getDis(startx, starty, endx, endy), startx, starty);
-            // 记录经过某点时的 步数，以该点二维坐标转化成的一维坐标为 key，步数为 value
-            map<int, int> mp;
-            mp[startx * n + starty] = 0;
+            // 记录经过某点时的 步数
+            vector<vector<int>> vis(m, vector<int>(n, INT_MAX));
+            vis[startx][starty] = 0;
             while (!pq.empty()) {
                 // 技巧！！！
                 auto [_, curx, cury] = pq.top();
-                // 由于不是顺序遍历，所以要取出该点的 步数
-                step = mp[curx * n + cury] + 1;
+                // 由于不是顺序遍历，所以要取出该点的 步数，+1 为当前步数
+                step = vis[curx][cury] + 1;
                 pq.pop();
                 // 四个方向遍历
                 for (int j = 0; j < 4; ++j) {
@@ -52,26 +92,25 @@ public:
                     if (nx < 0 || nx >= m || ny < 0 || ny >= n || forest[nx][ny] == 0) continue;
                     // 如果记录的 （经过该点时的步数） 小于等于 当前步数，则跳过，无需继续
                     // 否则可以继续遍历该点，更新结果
-                    // 注意要先检查是否存在！！！否则直接访问不存在的值，会直接插入 0
-                    if (mp.count(nx * n + ny) != 0 && mp[nx * n + ny] <= step) continue;
+                    if (vis[nx][ny] <= step) continue;
                     // 注意这里如果 start == end，会找不到结果
                     // 但是一开头检查了这种情况，所以这里写在这里
                     if (nx == endx && ny == endy) return step;
                     pq.emplace(step + getDis(nx, ny, endx, endy), nx, ny);
-                    mp[nx * n + ny] = step;
+                    vis[nx][ny] = step;
                 }
             }
             return -1;
         };
         int res = 0;
         int cx = 0, cy = 0;
-        for (int i = 0; i < tree.size(); ++i) {
-            int d = astar(cx, cy, tree[i].first, tree[i].second);
+        for (int i = 0; i < trees.size(); ++i) {
+            int d = bfs(cx, cy, trees[i].first, trees[i].second);
             if (d == -1) return -1;
             res += d;
             // 这里用到了一个小技巧，把比较合并在一个 for 循环
-            cx = tree[i].first;
-            cy = tree[i].second;
+            cx = trees[i].first;
+            cy = trees[i].second;
         }
         return res;
     }
