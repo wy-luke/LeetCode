643d678f45780abe02a87606a8c42a4c8dabf68f
wy-luke <li1078797083@qq.com>
2022-05-25 18:58:45 +0800

    重写704

--- 704.二分查找.cpp
+++ 704.二分查找.cpp
@@ -8,16 +8,15 @@
 class Solution {
 public:
     int search(vector<int> &nums, int target) {
-        int left = 0, right = nums.size() - 1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
+        int l = 0, r = nums.size();
+        while (l < r) {
+            int mid = l + ((r - l) >> 1);
             if (nums[mid] == target) {
                 return mid;
             } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else if (nums[mid] > target) {
-                right = mid - 1;
+                l = mid + 1;
+            } else {
+                r = mid;
             }
         }
         return -1;
