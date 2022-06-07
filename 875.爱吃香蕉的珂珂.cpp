581b308272cb0320feb56bb52d472a78da2a4579
wy-luke <li1078797083@qq.com>
2022-06-07 17:03:43 +0800

    除法向上取整

--- 875.爱吃香蕉的珂珂.cpp
+++ 875.爱吃香蕉的珂珂.cpp
@@ -10,25 +10,25 @@ public:
     // 耗时和速度的单调递减函数
     int f(vector<int> &piles, int speed) {
         int hours = 0;
-        for (int i : piles) {
-            hours += i / speed;
-            if (i % speed != 0) ++hours;
+        for (int pile : piles) {
+            // 向上取整
+            hours += (pile + speed - 1) / speed;
         }
         return hours;
     }
 
     int minEatingSpeed(vector<int> &piles, int h) {
-        // 定义最小值和最大值，区间为左闭右开 [ )，所以 right 要 +1
+        // 定义最小值和最大值
         int left = 1;
-        int right = *max_element(piles.begin(), piles.end()) + 1;
+        int right = *max_element(piles.begin(), piles.end());
 
         // 寻找左侧边界的二分查找，注意函数为单调递减
-        while (left < right) {
+        while (left <= right) {
             int mid = left + (right - left) / 2;
             if (f(piles, mid) > h) {
                 left = mid + 1;
             } else {
-                right = mid;
+                right = mid - 1;
             }
         }
         return left;
