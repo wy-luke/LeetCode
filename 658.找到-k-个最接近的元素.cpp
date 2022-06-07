59aed7f313bb78513f65e90d557824698dd48efd
wy-luke <li1078797083@qq.com>
2022-06-07 17:55:39 +0800

    简单优化

--- 658.找到-k-个最接近的元素.cpp
+++ 658.找到-k-个最接近的元素.cpp
@@ -9,25 +9,25 @@ class Solution {
 public:
     vector<int> findClosestElements(vector<int> &arr, int k, int x) {
         int n = arr.size();
+        // 二分
         auto it = lower_bound(arr.begin(), arr.end(), x);
         int idx = (int)(it - arr.begin());
-        vector<int> res;
+        // 双指针
         int l = idx - 1, r = idx;
-        while (res.size() < k) {
+        while (r - l - 1 < k) {
             if (l >= 0 && r < n) {
                 if (abs(arr[l] - x) <= abs(arr[r] - x)) {
-                    res.emplace_back(arr[l--]);
+                    l--;
                 } else {
-                    res.emplace_back(arr[r++]);
+                    r++;
                 }
             } else if (l < 0) {
-                res.emplace_back(arr[r++]);
+                r++;
             } else {
-                res.emplace_back(arr[l--]);
+                l--;
             }
         }
-        sort(res.begin(), res.end());
-        return res;
+        return vector<int>(arr.begin() + l + 1, arr.begin() + r);
     }
 };
 // @lc code=end
