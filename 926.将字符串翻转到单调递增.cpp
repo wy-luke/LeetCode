e0671882357cff8c29d0bc619abff8b7547dc2a8
wy-luke <li1078797083@qq.com>
2022-06-11 12:40:38 +0800

    前缀和完成926

--- 926.将字符串翻转到单调递增.cpp
+++ 926.将字符串翻转到单调递增.cpp
@@ -8,25 +8,21 @@
 class Solution {
 public:
     int minFlipsMonoIncr(string s) {
-        // cnt0 为单调递增且以 0 结尾时的最小翻转次数，cnt1 为以 1 结尾
-        int cnt0 = 0, cnt1 = 0;
-        for (char c : s) {
-            if (c == '0') {
-                // 当前位为 0 时
-                // 以 0 结尾，前一位必须是 0，所以 cnt0 等于上一位 cnt0
-                // 以 1 结尾，前一位可以是 0 或 1，取小的，并且当前位要翻转为 1，所以 +1
-                cnt1 = min(cnt0, cnt1) + 1;
-            } else {
-                // 当前位为 1 时
-                // 以 1 结尾，前一位可以是 0 或 1，取小的，当前位不需翻转
-                // 因为这里要用到 cnt0，所以先更新 cnt1
-                cnt1 = min(cnt0, cnt1);
-                // 以 0 结尾，前一位必须是 0，且当前位需要翻转为 0，所以 +1
-                cnt0 = cnt0 + 1;
-            }
+        // 前缀和
+        int n = s.size();
+        vector<int> pre(n + 1);
+        for (int i = 0; i < n; ++i) {
+            pre[i + 1] = pre[i] + (s[i] - '0');
         }
-        // 返回到最后一位时，以 0 或 1 结尾中翻转次数小的
-        return min(cnt0, cnt1);
+        int res = INT_MAX;
+        for (int i = 0; i <= n; ++i) {
+            // 计算当 i 左侧（不含）为 0，i 右侧（包含）为 1 时的翻转次数
+            // pre[i] 为 i 左侧的和，即有多少个 1，即翻转为 0 需要的次数
+            // n-i 为 i 右侧共有的数字个数，pre[n]-pre[i] 为 i 右侧 1 的个数，差即为 0 的个数，即翻转为 1 需要的次数
+            // 两者和为总共需要翻转的次数
+            res = min(res, pre[i] + (n - i) - (pre[n] - pre[i]));
+        }
+        return res;
     }
 };
 // @lc code=end
