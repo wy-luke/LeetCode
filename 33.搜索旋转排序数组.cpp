db0366b43a3c83379681accfcde92195f510860c
wy-luke <li1078797083@qq.com>
2022-06-17 14:02:52 +0800

    二次二分33

--- 33.搜索旋转排序数组.cpp
+++ 33.搜索旋转排序数组.cpp
@@ -8,28 +8,30 @@
 class Solution {
 public:
     int search(vector<int> &nums, int target) {
-        int l = 0, r = nums.size();
+        int n = nums.size();
+        int l = 0, r = n;
+        // 先查找分界点
         while (l < r) {
             int mid = l + ((r - l) >> 1);
-            // 当目标在左边部分
-            if (target >= nums[0]) {
-                // 当 mid 在左边部分，且值小于 target 时，向右收缩
-                // 当 mid 在右边部分，或在左边部分且值 大于等于 target 时，向左收缩
-                // 最后结果是第一个大于等于 target 的下标
-                if (nums[mid] >= nums[0] && nums[mid] < target) {
-                    l = mid + 1;
-                } else {
-                    r = mid;
-                }
-            } else { // 当目标在右边部分
-                // 当 mid 在左边部分，或值小于 target 时，向右收缩
-                // 当 mid 在右边部分，且值 大于等于 target 时，向左收缩
-                // 最后结果是第一个大于等于 target 的下标
-                if (nums[mid] >= nums[0] || nums[mid] < target) {
-                    l = mid + 1;
-                } else {
-                    r = mid;
-                }
+            if (nums[mid] >= nums[0]) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+        // 把区间变为目标所在区间
+        if (target >= nums[0]) {
+            l = 0;
+        } else {
+            r = n;
+        }
+        // 在目标区间搜索
+        while (l < r) {
+            int mid = l + ((r - l) >> 1);
+            if (nums[mid] < target) {
+                l = mid + 1;
+            } else {
+                r = mid;
             }
         }
         if (l == nums.size() || nums[l] != target) {
