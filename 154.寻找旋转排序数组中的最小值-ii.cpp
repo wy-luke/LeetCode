007fcf7a6c114f45272caf2f0828e395efa64961
wy-luke <li1078797083@qq.com>
2022-06-17 18:10:49 +0800

    逻辑稍微优化154

--- 154.寻找旋转排序数组中的最小值-ii.cpp
+++ 154.寻找旋转排序数组中的最小值-ii.cpp
@@ -13,9 +13,7 @@ public:
         // 保证数组二段性，即左部分 >=nums[0]，右部分 <nums[0]
         while (r >= 0 && nums[r] == nums[0]) --r;
         // 此时 nums[r]!=nums[0]，如果 nums[r]>=nums[0]说明只有左部分，否则说明有右部分
-        int tmp = r;
-        // [0,tmp] 可以理解为我们我们的新查找区间，下标 tmp+1 相当于 .end()
-        // 现在 [0,tmp] 内查找分界点
+        // 现在 [0,r] 内查找分界点
         while (l <= r) {
             int mid = l + ((r - l) >> 1);
             if (nums[mid] >= nums[0]) {
@@ -24,14 +22,12 @@ public:
                 r = mid - 1;
             }
         }
-        // 此时 r<l，l为右部分第一个，r为左部分最后一个
-        // l==tmp+1 相当于找到了 nums.end()，也就是没有右部分，所以 nums[0] 就是最小
-        // 否则，l 就是右部分第一个，就是最小的元素
-        if (l == tmp + 1) {
-            return nums[0];
-        } else {
-            return nums[l];
-        }
+        // 理论上 l 为右部分第一个
+        // l != n 时：
+        // 当存在右部分，l 就是右部分第一个，即为最小值
+        // 当不存在右部分，说明原数组右部分都是和 nums[0] 相同的，nums[l] 就是 nums[0]，即为最小值
+        // l==n 时，说明不存在右部分，nums[0] 即为最小值
+        return l == n ? nums[0] : nums[l];
     }
 };
 // @lc code=end
