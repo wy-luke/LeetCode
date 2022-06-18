4c64e5992d7bc3be47c0f042a8c3d55fd854af0c
wy-luke <li1078797083@qq.com>
2022-06-18 14:25:24 +0800

    优化二分2300

--- 2300.咒语和药水的成功对数.cpp
+++ 2300.咒语和药水的成功对数.cpp
@@ -10,19 +10,9 @@ public:
     vector<int> successfulPairs(vector<int> &spells, vector<int> &potions, long long success) {
         int n = spells.size(), m = potions.size();
         sort(potions.begin(), potions.end());
-
-        auto getCnt = [&](int spell) -> int {
-            int l = 0, r = m;
-            while (l < r) {
-                int mid = l + ((r - l) >> 1);
-                if ((long long)spell * potions[mid] < success) {
-                    l = mid + 1;
-                } else {
-                    r = mid;
-                }
-            }
-            return m - l;
-        };
+        // 设 spell 为 x，potion 为 y
+        // x*y>=success 等价于 y>=⌈success/x⌉ 就等价于 y>(success−1​)/x
+        // ⌈success/x⌉ = (success+x-1)/x = (success-1)/x+1 > (success−1​)/x
         unordered_map<int, int> mp;
         vector<int> res;
         res.reserve(n);
@@ -30,7 +20,7 @@ public:
             if (mp.count(spells[i])) {
                 res.emplace_back(mp[spells[i]]);
             } else {
-                int cnt = getCnt(spells[i]);
+                int cnt = potions.end() - upper_bound(potions.begin(), potions.end(), (success - 1) / spells[i]);
                 res.emplace_back(cnt);
                 mp[spells[i]] = cnt;
             }
