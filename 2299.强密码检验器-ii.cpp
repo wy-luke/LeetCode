9134b239c00d3cdce8a3b55fdc766dca7833fc78
wy-luke <li1078797083@qq.com>
2022-06-18 13:52:04 +0800

    stl2299

--- 2299.强密码检验器-ii.cpp
+++ 2299.强密码检验器-ii.cpp
@@ -13,9 +13,9 @@ public:
         string s = "!@#$%^&*()-+";
         bool b2 = false, b3 = false, b4 = false, b5 = false, b6 = true;
         for (int i = 0; i < n; ++i) {
-            if (!b2 && password[i] - 'a' >= 0 && password[i] - 'a' < 26) b2 = true;
-            if (!b3 && password[i] - 'A' >= 0 && password[i] - 'A' < 26) b3 = true;
-            if (!b4 && password[i] - '0' >= 0 && password[i] - '0' < 10) b4 = true;
+            if (!b2 && islower(password[i])) b2 = true;
+            if (!b3 && isupper(password[i])) b3 = true;
+            if (!b4 && isdigit(password[i])) b4 = true;
             if (!b5 && s.find(password[i]) != string::npos) b5 = true;
             if (i < n - 1 && password[i] == password[i + 1]) b6 = false;
         }
