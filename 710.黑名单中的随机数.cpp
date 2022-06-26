d1a7a65e04312b1b300e91b6905837068927ba7a
wy-luke <li1078797083@qq.com>
2022-06-26 13:39:12 +0800

    代码优化710

--- 710.黑名单中的随机数.cpp
+++ 710.黑名单中的随机数.cpp
@@ -7,7 +7,7 @@
 // @lc code=start
 class Solution {
 private:
-    // 哈希表记录 数字 -> index, 用于将 黑名单数字 映射到 正常数字
+    // 将 黑名单数字 映射到 正常数字
     unordered_map<int, int> mp;
     // 记录除去黑名单后的数字个数
     int sz;
@@ -15,30 +15,23 @@ private:
 public:
     Solution(int n, vector<int> &blacklist) {
         sz = n - blacklist.size();
-
-        for (int i : blacklist) {
-            mp.emplace(i, i);
+        for (int b : blacklist) {
+            mp[b] = b;
         }
-
-        int right = n - 1;
-        for (int i : blacklist) {
-            if (i >= sz) continue;
-
-            // 如果 right 在 blacklist 中，直接左移，直到找到非黑名单数字
-            while (mp.count(right) != 0) {
-                right--;
+        int i = sz;
+        for (int b : blacklist) {
+            if (b < sz) {
+                while (mp.count(i)) {
+                    ++i;
+                }
+                mp[b] = i++;
             }
-            // 将 黑名单数字 映射到 正常数字
-            mp[i] = right;
-            right--;
         }
     }
 
     int pick() {
-        int x = rand() % sz;
-        // 如果随机数 x 为黑名单数字，则映射到正常数字
-        if (mp.count(x) != 0) return mp[x];
-        return x;
+        int rnd = rand() % sz;
+        return mp.count(rnd) ? mp[rnd] : rnd;
     }
 };
 
