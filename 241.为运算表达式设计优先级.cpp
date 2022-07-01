ee49a64a9dee16f34530088a9d0d29d588801e73
wy-luke <li1078797083@qq.com>
2022-07-01 17:03:05 +0800

    递归分治241🐮

---
+++ 241.为运算表达式设计优先级.cpp
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=241 lang=cpp
+ *
+ * [241] 为运算表达式设计优先级
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> diffWaysToCompute(string expression) {
+        int n = expression.size();
+        vector<int> res;
+        for (int i = 0; i < n; ++i) {
+            char c = expression[i];
+            if (isdigit(c)) continue;
+            vector<int> left = diffWaysToCompute(expression.substr(0, i));
+            vector<int> right = diffWaysToCompute(expression.substr(i + 1));
+            for (int l : left) {
+                for (int r : right) {
+                    if (c == '-') {
+                        res.emplace_back(l - r);
+                    } else if (c == '+') {
+                        res.emplace_back(l + r);
+                    } else {
+                        res.emplace_back(l * r);
+                    }
+                }
+            }
+        }
+        if (res.empty()) return {stoi(expression)};
+        return res;
+    }
+};
+// @lc code=end
