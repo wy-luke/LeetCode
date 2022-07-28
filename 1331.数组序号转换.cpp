863e01f342d4e2ea37aa8dcc77a7a36c4213ac72
wy-luke <li1078797083@qq.com>
2022-07-28 16:13:58 +0800

    逻辑优化1331

--- 1331.数组序号转换.cpp
+++ 1331.数组序号转换.cpp
@@ -8,19 +8,15 @@
 class Solution {
 public:
     vector<int> arrayRankTransform(vector<int> &arr) {
-        unordered_map<int, vector<int>> mp;
-        for (int i = 0; i < arr.size(); ++i) {
-            mp[arr[i]].emplace_back(i);
-        }
         vector<int> tmp = arr;
         sort(tmp.begin(), tmp.end());
+        unordered_map<int, int> mp;
         int idx = 1;
-        for (int i = 0; i < arr.size();) {
-            for (int j : mp[tmp[i]]) {
-                arr[j] = idx;
-            }
-            i += mp[tmp[i]].size();
-            ++idx;
+        for (int i : tmp) {
+            if (!mp.count(i)) mp[i] = idx++;
+        }
+        for (int i = 0; i < arr.size(); ++i) {
+            arr[i] = mp[arr[i]];
         }
         return arr;
     }
