eb6a043a9790091cafd878de6939921141d0b3d1
wy-luke <li1078797083@qq.com>
2022-08-20 19:24:09 +0800

    单调栈654

--- 654.最大二叉树.cpp
+++ 654.最大二叉树.cpp
@@ -16,17 +16,46 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     TreeNode *constructMaximumBinaryTree(vector<int> &nums) {
-        return helper(nums, 0, nums.size());
-    }
+        int n = nums.size();
+        stack<int> stk;
+        vector<int> l(n, -1), r(n, -1);
+        vector<TreeNode *> trees(n);
+        for (int i = 0; i < nums.size(); ++i) {
+            trees[i] = new TreeNode(nums[i]);
+            int num = nums[i];
+            while (!stk.empty() && num > nums[stk.top()]) {
+                r[stk.top()] = i;
+                stk.pop();
+            }
+            l[i] = stk.empty() ? -1 : stk.top();
+            stk.emplace(i);
+        }
 
-    TreeNode *helper(vector<int> &nums, int p, int q) {
-        if (p == q) return nullptr;
-
-        int idx = max_element(nums.begin() + p, nums.begin() + q) - nums.begin();
-        return new TreeNode(nums[idx], helper(nums, p, idx), helper(nums, idx + 1, q));
+        TreeNode *root = nullptr;
+        for (int i = 0; i < n; ++i) {
+            if (l[i] == -1 && r[i] == -1) {
+                root = trees[i];
+            } else if (l[i] == -1 || (r[i] != -1 && nums[r[i]] < nums[l[i]])) {
+                trees[r[i]]->left = trees[i];
+            } else {
+                trees[l[i]]->right = trees[i];
+            }
+        }
+        return root;
     }
 };
 // @lc code=end
