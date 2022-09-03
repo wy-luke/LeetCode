81620320f2a4d94d30b26cdc223c7958233eb64b
wy-luke <li1078797083@qq.com>
2022-09-03 21:19:43 +0800

    二分+贪心dp

--- 646.最长数对链.cpp
+++ 646.最长数对链.cpp
@@ -24,4 +24,29 @@ public:
         return ans;
     }
 };
+
+// 相似解法 300. 最长递增子序列
+class Solution2 {
+public:
+    int findLongestChain(vector<vector<int>> &pairs) {
+        sort(pairs.begin(), pairs.end());
+
+        int n = pairs.size();
+        // arr[i] 为长度是 i+1 的对数链的最小结尾 y
+        // 记录最小是因为这样可以有更大机会构造更长对数链
+        vector<int> arr;
+        for (auto &pair : pairs) {
+            int x = pair[0], y = pair[1];
+            if (arr.size() == 0 || x > arr.back()) {
+                arr.emplace_back(y);
+            } else {
+                // 找到当前数对可以插入的位置
+                int idx = lower_bound(arr.begin(), arr.end(), x) - arr.begin();
+                arr[idx] = min(arr[idx], y);
+            }
+        }
+        return arr.size();
+    }
+    return arr.size();
+};
 // @lc code=end
