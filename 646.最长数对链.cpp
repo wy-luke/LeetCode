82204d06c3bf0d419428b3a908d46db90193f0e5
wy-luke <li1078797083@qq.com>
2022-09-03 20:20:27 +0800

    dp+贪心646

--- 646.最长数对链.cpp
+++ 646.最长数对链.cpp
@@ -10,18 +10,16 @@ public:
     int findLongestChain(vector<vector<int>> &pairs) {
         sort(pairs.begin(), pairs.end());
 
-        int n = pairs.size();
-        vector<int> cnts(n);
-        int ans = 0;
-        for (int i = n - 1; i >= 0; --i) {
-            int cnt = 1;
-            for (int j = i + 1; j < n; ++j) {
-                if (pairs[j][0] > pairs[i][1]) {
-                    cnt = max(cnt, 1 + cnts[j]);
+        int n = pairs.size(), ans = 1;
+        vector<int> cnts(n, 1);
+        for (int i = 0; i < n; i++) {
+            for (int j = i - 1; j >= 0; --j) {
+                if (pairs[j][1] < pairs[i][0]) {
+                    cnts[i] = 1 + cnts[j];
+                    break;
                 }
             }
-            ans = max(ans, cnt);
-            cnts[i] = cnt;
+            ans = max(ans, cnts[i]);
         }
         return ans;
     }
