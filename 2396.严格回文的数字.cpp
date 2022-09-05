de4b05e88fd056d47fec7bf4edbf7a9c78922e10
wy-luke <li1078797083@qq.com>
2022-09-05 19:25:35 +0800

    脑筋急转弯2396

---
+++ 2396.严格回文的数字.cpp
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=2396 lang=cpp
+ *
+ * [2396] 严格回文的数字
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool isStrictlyPalindromic(int n) {
+        return false;
+    }
+};
+// @lc code=end
